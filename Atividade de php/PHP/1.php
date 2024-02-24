01d117849228408a617c13cf2999f694f3a30aca
Marco Antonio <marcotonho38@gmail.com>
2024-02-24 13:21:59 -0300

    ADD Soma simples PHP

---
+++ Atividade de php/PHP/1.php
@@ -0,0 +1,11 @@
+<?php
+// Definindo duas variáveis com valores
+$numero1 = 10;
+$numero2 = 5;
+
+// Realizando a adição
+$soma = $numero1 + $numero2;
+
+// Exibindo o resultado
+echo "A soma de $numero1 + $numero2 é igual a: $soma";
+?>
